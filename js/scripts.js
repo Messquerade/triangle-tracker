5c3c6fa6bf9822219e8ea71a30f036fcb7589d74
Messquerade <annac.klingberg@gmail.com>
2021-08-12 14:59:14 -0700

    add and hide results

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+
+
+
+
 $(document).ready(function() {
-  $("#")
-})
+  $("#triangle-specs").submit(function(event) {
+    event.preventDefault();
+    const side1 = parseFloat($("input#side1").val());
+    const side2 = parseFloat($("input#side2").val());
+    const side3 = parseFloat($("input#side3").val());
+  });
+});
